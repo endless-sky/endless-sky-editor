71ee8b06ff3c41945de1baffde96381a8876f77f
Michael Zahniser <mzahniser@gmail.com>
2015-05-14 21:07:40 -0400

    Added a man page, a desktop file, and --help and --version messages.

--- main.cpp
+++ main.cpp
@@ -17,18 +17,71 @@ PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 #include <QApplication>
 #include <QString>
 
+#include <iostream>
+
 using namespace std;
+
+void PrintHelp();
+void PrintVersion();
 
 
 
 int main(int argc, char *argv[])
 {
+    QString path;
+    for(const char *const *it = argv + 1; *it; ++it)
+    {
+        QString arg = *it;
+        if(arg == "-v" || arg == "--version")
+        {
+            PrintVersion();
+            return 0;
+        }
+        else if(arg[0] != '-')
+            path = arg;
+        else
+        {
+            PrintHelp();
+            return 0;
+        }
+    }
+
     QApplication a(argc, argv);
     Map mapData;
+    if(!path.isEmpty())
+        mapData.Load(path);
 
     MainWindow w(mapData);
     w.resize(1200, 900);
     w.show();
 
     return a.exec();
+}
+
+
+
+void PrintHelp()
+{
+    cerr << endl;
+    cerr << "Command line options:" << endl;
+    cerr << "    -h, --help: print this help message." << endl;
+    cerr << "    -v, --version: print version information." << endl;
+    cerr << "    <path to map.txt>: load the given map file." << endl;
+    cerr << "        Sprites are then loaded from ../images/ relative to the map file." << endl;
+    cerr << endl;
+    cerr << "Report bugs to: mzahniser@gmail.com" << endl;
+    cerr << "Home page: <https://endless-sky.github.io>" << endl;
+    cerr << endl;
+}
+
+
+
+void PrintVersion()
+{
+    cerr << endl;
+    cerr << "Endless Sky Editor v. 0.7.11" << endl;
+    cerr << "License GPLv3+: GNU GPL version 3 or later: <https://gnu.org/licenses/gpl.html>" << endl;
+    cerr << "This is free software: you are free to change and redistribute it." << endl;
+    cerr << "There is NO WARRANTY, to the extent permitted by law." << endl;
+    cerr << endl;
 }
